f6f257814a4c59ff7b12858b74b6e5a2a7efa1eb
Drew Frank <drewfrank@gmail.com>
2012-03-20 10:11:08 -0700

    Added a warning / prompt to the install script.

--- install.sh
+++ install.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+echo "WARNING: This script is about to place symlinks in your home directory 
+pointing to each file in this directory. If a file of the same name already 
+exists in your home directory, a backup will be made with suffix \".bak\""
+echo "";
+read -p "Do you want to continue? (y/n) " RESP
+
+if [ $RESP != "y" ]; then
+    echo "Goodbye :)"
+    exit;
+fi
+
 for file in $(ls -A); do
     if [ "$file" != "install.sh" ]; then
         if [ -e "$HOME/$file" -a ! -L "$HOME/$file" ]; then
